9db7afaf9363c312d3a6ecbf5fac70f3a088cbec
Gonbriel <gonbriel03@gmail.com>
2025-01-29 16:02:37 -0300

    Updating search_web.py for return json

--- services/search_web.py
+++ services/search_web.py
@@ -4,5 +4,20 @@ import os
 async def search_web(query: str) -> str:
     """ Useful for using the web to answer questions. """
     print('Searching web...\n')
+
     client = AsyncTavilyClient(api_key=str(os.getenv('TAVILY_KEY')))
-    return str(await client.search(query))
+
+    try:
+        result = await client.search(query, search_depth='basic', max_results=3)
+
+        structured_result = {
+            "query": query,
+            "top_results": [
+                {"title": item['title'], "url": item['url'], "snippet": item['content']}
+                for item in result['results']
+            ]
+        }
+        return structured_result
+    
+    except Exception as e:
+        return {'error': str(e)}
